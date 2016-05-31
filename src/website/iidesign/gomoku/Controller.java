fc1c8d4913a992201d6a75f956ff064700bb1922
kotaroyamame <yamame@fi.chu.jp>
2016-05-30 22:39:17 -0400

    整形、コメント追加

--- src/website/iidesign/gomoku/Controller.java
+++ src/website/iidesign/gomoku/Controller.java
@@ -12,7 +12,7 @@ import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
-public class Controller  implements Initializable {
+public class Controller implements Initializable {
 	@FXML
 	private Canvas canvas;
 	@FXML
@@ -22,42 +22,45 @@ public class Controller  implements Initializable {
 	GraphicsContext gc;
 	Bord bord;
 	Shinpan shinpan;
-	private boolean finish=false;
+	private boolean finish = false;
 	private Computer com;
 
 	@Override
-  public void initialize(URL location, ResourceBundle resources) {
-		
+	public void initialize(URL location, ResourceBundle resources) {
+
 		init();
 	}
-	
-	private void init(){
-		gc=canvas.getGraphicsContext2D();
-		shinpan=new Shinpan();
-		bord=new Bord(gc);
-		finish=false;
+
+	private void init() {
+		gc = canvas.getGraphicsContext2D();
+		shinpan = new Shinpan();
+		bord = new Bord(gc);
+		finish = false;
 		text1.setText("");
-		com=new Computer();
+		com = new Computer();
 	}
+
 	@FXML
-	private void onRestart(){
+	private void onRestart() {
 		this.init();
 	}
+
 	@FXML
-	private void clickCanvas(MouseEvent e){
-		if(finish)return ;
-		int _x=(int)Math.floor(e.getX())/bord.SIZE;
-		int _y=(int)Math.floor(e.getY())/bord.SIZE;
-		if(bord.setStorn(_x,_y,true))
-			if(shinpan.hantei(_x,_y,true)){
-				text1.setText("先手の勝ちです");
-				finish=true;
+	private void clickCanvas(MouseEvent e) {
+		if (finish)
+			return;
+		int _x = (int) Math.floor(e.getX()) / bord.SIZE;
+		int _y = (int) Math.floor(e.getY()) / bord.SIZE;
+		if (bord.setStorn(_x, _y, true))
+			if (shinpan.hantei(_x, _y, true)) {
+				text1.setText("あなたの勝ちです");
+				finish = true;
 			}
-		int[] comStone=com.setStorn();
-		if(bord.setStorn(comStone[0],comStone[1],false))
-			if(shinpan.hantei(comStone[0],comStone[1],false)){
-				text1.setText("後手の勝ちです");
-				finish=true;
+		int[] comStone = com.setStorn();
+		if (bord.setStorn(comStone[0], comStone[1], false))
+			if (shinpan.hantei(comStone[0], comStone[1], false)) {
+				text1.setText("コンピュータの勝ちです");
+				finish = true;
 			}
 	}
 

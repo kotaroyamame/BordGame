962bf87f8174b1bce1a5ec2076c26ae40d0be5bc
kotaroyamame <yamame@fi.chu.jp>
2016-05-30 22:20:35 -0400

    連結が多い石の両端を狙うアルゴリズム追加

--- src/website/iidesign/gomoku/Computer.java
+++ src/website/iidesign/gomoku/Computer.java
@@ -11,6 +11,55 @@ public class Computer {
 		serchStone();
 	}
 
+	private int[] sarch(int x, int y, int val, int pt) {
+		switch (pt) {
+		case 1:
+			return new int[] { Bord.getStorn(x + val, y), x + val, y };
+		case 2:
+			return new int[] { Bord.getStorn(x - val, y), x - val, y };
+		case 3:
+			return new int[] { Bord.getStorn(x + val, y + val), x + val, y + val };
+		case 4:
+			return new int[] { Bord.getStorn(x - val, y - val), x - val, y - val };
+		case 5:
+			return new int[] { Bord.getStorn(x + val, y - val), x + val, y - val };
+		case 6:
+			return new int[] { Bord.getStorn(x - val, y + val), x - val, y + val };
+		case 7:
+			return new int[] { Bord.getStorn(x, y + val), x, y + val };
+		case 8:
+			return new int[] { Bord.getStorn(x, y - val), x, y - val };
+		default:
+			return new int[] { -1, 0, 0 };
+		}
+	}
+
+	private int[] ifThree(int x, int y, boolean br) {
+		int storn = br ? 0 : 1;
+		int count[]={0,0};
+		out: for (int i = 1; i <= 8; i++) {//八方
+			count[0]=0;
+			for (int j = 1; j < 3; j++) {//n回並んでいる石
+				
+				if (this.sarch(x, y, j, i)[0] == storn && j == 2-count[1]) {
+					if(this.sarch(x, y, j+1, i)[0]==-1){
+						return this.sarch(x, y, j+1, i);
+					}else if(this.sarch(x, y, 0, i)[0]==-1){
+						return this.sarch(x, y, 0, i);
+					}
+				} else if (this.sarch(x, y, j, i)[0] == storn) {
+					count[0]++;
+				} else {
+					count[1]=i%2==0?0:count[0];
+					count[0]=0;
+					continue out;
+				}
+			}
+		}
+
+		return new int[]{-2,0,0};
+	}
+
 	private void serchStone() {
 		myStone.clear();
 		humanStone.clear();
@@ -56,7 +105,7 @@ public class Computer {
 		case 1:
 			return new int[] { humanStone.get(i)[0] + 1, humanStone.get(i)[1] - 1 };
 		case 2:
-			return new int[] { humanStone.get(i)[0]+1, humanStone.get(i)[1] };
+			return new int[] { humanStone.get(i)[0] + 1, humanStone.get(i)[1] };
 		case 3:
 			return new int[] { humanStone.get(i)[0] + 1, humanStone.get(i)[1] + 1 };
 		case 4:
@@ -72,14 +121,25 @@ public class Computer {
 	}
 
 	public int[] isRen(int l) {
-		out:for (int i = 0; i < humanStone.size(); i++) {
+		for (int i1 = 0; i1 < Bord.X; i1++) {
+			for (int j1 = 0; j1 < Bord.X; j1++) {
+				if (this.ifThree(i1, j1, false)!=null&this.ifThree(i1, j1, false)[0]==-1) {
+
+					return new int[]{this.ifThree(i1, j1, false)[1],this.ifThree(i1, j1, false)[2]};
+
+				}
+			}
+		}
+
+		out: for (int i = 0; i < humanStone.size(); i++) {
 
 			int[] xy;
 			int count = 0;
 			do {
 				xy = getHumanStoneBlank((int) Math.floor(Math.random() * 8), i);
 				count++;
-				if(count>3)continue out;
+				if (count > 3)
+					continue out;
 			} while (!isBlank(xy));
 
 			System.out.println(xy[0] + " " + xy[1]);

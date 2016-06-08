74fc8526b5cb0bf5e1436504c6635eb4b2410295
kotaroyamame <yamame@fi.chu.jp>
2016-06-08 02:18:28 -0400

    AI学習ボタン追加

--- src/website/iidesign/gomoku/Controller.java
+++ src/website/iidesign/gomoku/Controller.java
@@ -39,7 +39,7 @@ public class Controller implements Initializable {
 		finish = false;
 		text1.setText("");
 		com = new Computer();
-		ai=new AI();
+		ai = new AI();
 	}
 
 	@FXML
@@ -47,78 +47,110 @@ public class Controller implements Initializable {
 		this.init();
 	}
 
+
+	@FXML
+	public void onLearning() {
+		for (int i = 0; i < 20;i++) {
+			out:for (;;) {
+				if (finish)
+					break out;
+
+				aiRanch2();
+
+				if (finish)
+					return;
+				aiRanch();
+			}
+		this.init();
+		}
+	}
+
 	@FXML
 	private void clickCanvas(MouseEvent e) {
-//		for(;;){
-		if (finish)
-			return;
-		int _x = (int) Math.floor(e.getX()) / bord.SIZE;
-		int _y = (int) Math.floor(e.getY()) / bord.SIZE;
-		int hbr=shinpan.ifFoul(_x, _y, true);
-		if(hbr==-1){
-			text1.setText("あなたの番です");
+		// for(int i=0;i<20;i++){
+//		for (;;) {
+			if (finish)
+				return;
+			// ここから人
+
+			 int _x = (int) Math.floor(e.getX()) / bord.SIZE;
+			 int _y = (int) Math.floor(e.getY()) / bord.SIZE;
 			
-//			aiRanch();
-			
-			boolean hSet=bord.setStorn(_x, _y, true);
-			if (hSet){
-				if (shinpan.hantei(_x, _y, true)) {
-					text1.setText("あなたの勝ちです");
+			 int hbr=shinpan.ifFoul(_x, _y, true);
+			 if(hbr==-1){
+			 text1.setText("あなたの番です");
+
+			// ここまで人
+
+//			aiRanch2();
+
+			// ここから人
+
+			 boolean hSet=bord.setStorn(_x, _y, true);
+			 if (hSet){
+			 if (shinpan.hantei(_x, _y, true)) {
+			 text1.setText("あなたの勝ちです");
+			 finish = true;
+			 }
+			 }
+			 }else if(shinpan.ifFoul(_x, _y, true)==0){
+			 text1.setText("先手の三々は反則です");
+			 }
+
+			// ここまで人
+			if (finish)
+				return;
+			comRanch();
+//		}
+	}
+
+	private void comRanch() {
+		int[] comStone = com.setStorn();
+		boolean aSet = bord.setStorn(comStone[0], comStone[1], false);
+		if (aSet) {
+			if (shinpan.hantei(comStone[0], comStone[1], false)) {
+				text1.setText("コンピュータの勝ちです");
+				finish = true;
+			}
+		}
+	}
+
+	private void comRanch2() {
+		int[] comStone = com.setStorn();
+		int hbr = shinpan.ifFoul(comStone[0], comStone[1], true);
+		if (hbr == -1) {
+			boolean aSet = bord.setStorn(comStone[0], comStone[1], true);
+			if (aSet) {
+				if (shinpan.hantei(comStone[0], comStone[1], true)) {
+					text1.setText("コンピュータの勝ちです");
 					finish = true;
 				}
 			}
-		}else if(shinpan.ifFoul(_x, _y, true)==0){
-			text1.setText("先手の三々は反則です");
-		}
-		if (finish)
-			return;
-		comRanch();
-//		}
-	}
-	private void comRanch(){
-		int[] comStone = com.setStorn();
-		boolean aSet=bord.setStorn(comStone[0], comStone[1], false);
-		if (aSet){
-			if (shinpan.hantei(comStone[0], comStone[1], false)) {
-				text1.setText("コンピュータの勝ちです");
-				finish = true;
-			}
 		}
 	}
-	private void comRanch2(){
-		int[] comStone = com.setStorn();
-		int hbr=shinpan.ifFoul(comStone[0], comStone[1], true);
-		if(hbr==-1){
-		boolean aSet=bord.setStorn(comStone[0], comStone[1], true);
-		if (aSet){
-			if (shinpan.hantei(comStone[0], comStone[1], true)) {
-				text1.setText("コンピュータの勝ちです");
-				finish = true;
-			}
-		}
-		}
-	}
-	private void aiRanch(){
-		int[] comStone = ai.setStorn(bord.bord,false,bord.getTekazu());
-		boolean aSet=bord.setStorn(comStone[0], comStone[1], false);
-		if (aSet){
+
+	private void aiRanch() {
+		int[] comStone = ai.setStorn(bord.bord, false, bord.getTekazu());
+		boolean aSet = bord.setStorn(comStone[0], comStone[1], false);
+		if (aSet) {
 			if (shinpan.hantei(comStone[0], comStone[1], false)) {
 				text1.setText("AIの勝ちです");
 				finish = true;
 			}
 		}
 	}
-	private void aiRanch2(){
-		int[] comStone = ai.setStorn(bord.bord,false,bord.getTekazu());
-		int hbr=shinpan.ifFoul(comStone[0], comStone[1], true);
-		if(hbr==-1){
-		boolean aSet=bord.setStorn(comStone[0], comStone[1], true);
-		if (aSet){
-			if (shinpan.hantei(comStone[0], comStone[1], true)) {
-				text1.setText("AIの勝ちです");
-				finish = true;
+
+	private void aiRanch2() {
+		int[] comStone = ai.setStorn(bord.bord, false, bord.getTekazu());
+		int hbr = shinpan.ifFoul(comStone[0], comStone[1], true);
+		if (hbr == -1) {
+			boolean aSet = bord.setStorn(comStone[0], comStone[1], true);
+			if (aSet) {
+				if (shinpan.hantei(comStone[0], comStone[1], true)) {
+					text1.setText("AIの勝ちです");
+					finish = true;
+				}
 			}
-		}
 		}
 	}
 

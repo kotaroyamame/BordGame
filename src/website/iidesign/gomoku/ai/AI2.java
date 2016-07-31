b21fec8a2a9626cf779e88b991ecdaa4c07d14af
kotaroyamame <yamame@fi.chu.jp>
2016-07-31 02:48:40 -0400

    ###

--- src/website/iidesign/gomoku/ai/AI2.java
+++ src/website/iidesign/gomoku/ai/AI2.java
@@ -1,10 +1,45 @@
 package website.iidesign.gomoku.ai;
 
 public class AI2 {
-	private Node[] p;
+	private Node[] n;
+	//入力値
+	int[][] x={
+			{0,0},
+			{0,1},
+			{1,0},
+			{1,1},
+	};
+	//教師値
+	int[] y={
+			0,
+			1,
+			1,
+			1
+	};
 
 	public AI2() {
-
+		//ノードとWの初期化
+		this.n=new Node[1];
+		for(int i=0;i<n.length;i++){
+			n[i]=new Node(0);
+		}
+		n[0].setWeight(0,0);
+		
+		if(n[0].hog(x[0][0],x[0][1])==y[0]){
+			System.out.println("W値修正なし");
+		}else{
+			System.out.println("W値修正あり");
+		}
+		
+		
+	}
+	
+	public void sum(int... p){
+		for(int i=0;i<n.length;i++){
+			for(int j=0;j<p.length;j++){
+				n[i].setWeight(p[j]);
+			}
+		}
 	}
 
 }
--- src/website/iidesign/gomoku/ai/Node.java
+++ src/website/iidesign/gomoku/ai/Node.java
@@ -1,29 +1,38 @@
 package website.iidesign.gomoku.ai;
 
 public class Node {
-	private double w;
+	private double[] w;
 	private int theta=0;
 
-	public Node(double w) {
-		this.w=w;
+	public Node(double... w) {
+		this.w=new double[w.length];
+		for(int i=0;i<w.length;i++)
+			this.w[i]=w[i];
 	}
 	
-	public void setWeight(double w) {
-		this.w=w;
+	public void setWeight(double... w) {
+		this.w=new double[w.length];
+		for(int i=0;i<w.length;i++)
+			this.w[i]=w[i];
 	}
 	
-	public double getWeight() {
+	public double[] getWeight() {
 		return w;
 	}
 	
-	public int hog(double p){
-		int re;
-		if(p*w>=theta){
-			re=1;
-		}else{
-			re=0;
+	public int hog(double... p){
+		
+		int res=0;
+		int wp=0;
+		for(int i=0;i<p.length;i++){
+			wp+=p[i]*w[i];
 		}
-		return re; 
+		if(wp>=theta){
+			res=1;
+		}else{
+			res=0;
+		}
+		return res; 
 	}
 
 }
